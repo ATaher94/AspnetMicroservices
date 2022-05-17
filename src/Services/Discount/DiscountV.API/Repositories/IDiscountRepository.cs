7009c7b3e5671d7f6aebf3643f06ab77f168eabb
ATaher94 <61141486+ATaher94@users.noreply.github.com>
2022-05-17 13:36:57 +0300

    Commit With Push Changes

---
+++ src/Services/Discount/DiscountV.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using DiscountV.API.Entities;
+using System.Threading.Tasks;
+
+namespace DiscountV.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
